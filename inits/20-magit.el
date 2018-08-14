e52e93254728e7870261e3165beb00313a1cbbd8
INAMORI Takayuki <t.inamori@daisychain.jp>
2018-08-14 13:00:36 +0900

    install orgit again

--- inits/20-magit.el
+++ inits/20-magit.el
@@ -1,6 +1,8 @@
 (use-package magit
   :straight t
-  :bind (:map magit-log-mode-map
+  :bind (:map magit-status-mode-map
+              ("C-j" . magit-visit-thing)
+              :map magit-log-mode-map
               ("C-j" . magit-visit-thing))
   :config
   (add-hook 'magit-status-mode-hook
@@ -8,6 +10,5 @@
               (buffer-face-set 'selecting))))
 
 (use-package orgit
-  :disabled
   :straight t
   :after (magit org))
