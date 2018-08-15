30a22aab6a2cc93aff24e77758f2c27be503804d
INAMORI Takayuki <t.inamori@daisychain.jp>
2018-08-15 21:50:48 +0900

    switch to straight.el for flycheck

--- inits/30-flycheck.el
+++ inits/30-flycheck.el
@@ -1,5 +1,6 @@
-(el-get-bundle flycheck)
+;; (el-get-bundle flycheck)
 (use-package flycheck
+  :straight t
   :diminish (flycheck-mode . "fc")
   :config
   (bind-keys :map flycheck-error-list-mode-map
@@ -7,6 +8,5 @@
   (setq flycheck-check-syntax-automatically
         '(save new-line idle-change))
   (setq flycheck-display-errors-delay 60.0)
-  (setq flycheck-checker-error-threshold 1000))
-
-(add-hook 'c-mode-hook 'flycheck-mode)
+  (setq flycheck-checker-error-threshold 1000)
+  (add-hook 'c-mode-hook 'flycheck-mode))
