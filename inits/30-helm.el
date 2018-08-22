db78bd73e99a2c3d8bd6a9737a1c8d6adb5eef58
INAMORI Takayuki <t.inamori@daisychain.jp>
2018-08-22 16:41:15 +0900

    switch to el-get for helm since straight.el does not support building(make)

--- inits/30-helm.el
+++ inits/30-helm.el
@@ -1,9 +1,10 @@
+(el-get-bundle helm)
 (use-package helm
-  :straight t
   :bind (("C-;"     . helm-mini)
          ("C-M-;"   . helm-for-files)
          ("M-x"     . helm-M-x)
          ("C-x C-f" . helm-find-files)
+         ("C-c C-b" . helm-bookmarks)
          ("M-y"     . helm-show-kill-ring)
          ("C-M-SPC" . helm-all-mark-rings)
          ("C-c i"   . helm-imenu)
