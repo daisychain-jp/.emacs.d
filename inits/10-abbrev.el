2ab8031815421579d31c7765c2df2d1df5ccfeb3
INAMORI Takayuki <t.inamori@daisychain.jp>
2018-10-14 11:01:50 +0900

    add some candidate list in hippie expand

--- inits/10-abbrev.el
+++ inits/10-abbrev.el
@@ -10,5 +10,7 @@
         try-complete-file-name
         try-expand-dabbrev
         try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill))
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
 (global-set-key (kbd "M-/") 'hippie-expand)
