e2770a3747e500bcbdd361d31f0439843370f115
INAMORI Takayuki <t.inamori@daisychain.jp>
2018-12-02 18:23:27 +0900

    get rid of inactive timestamp entries from log mode view

--- inits/50-org-life.el
+++ inits/50-org-life.el
@@ -52,7 +52,7 @@
            agenda ""
            ((org-agenda-span 'day)
             (org-agenda-start-with-log-mode t)
-            (org-agenda-include-inactive-timestamps t)
+            (org-agenda-include-inactive-timestamps nil)
             (org-agenda-include-diary t)
             (org-agenda-sorting-strategy
              '(time-up
