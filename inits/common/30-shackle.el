7965410093a8080d4e16a834712fb93e54586313
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-02-28 18:26:51 +0900

    pop up error massage buffer of flycheck instead appearing as a full window

--- inits/common/30-shackle.el
+++ inits/common/30-shackle.el
@@ -1,16 +1,18 @@
 (use-package shackle
   :straight t
+  :after (flycheck)
+  :custom
+  (shackle-rules
+   `(('undo-tree-visualizer-mode :popup t)
+     ('sql-interactive-mode :popup t)
+     ('magit-diff-mode :below t)
+     (,flycheck-error-message-buffer :below t)
+     ("*shell*" :same t)
+     ("*Help*" :same t)
+     ("*Apropos*" :same t)
+     ("*el-get packages*" :same t)
+     ("*pry*" :same t)
+     ("j-\*" :regexp t :same t)))
   :config
   (custom-set-variables '(shackle-default-rule '(:same t)))
-  (custom-set-variables
-   '(shackle-rules
-     '(('undo-tree-visualizer-mode :popup t)
-       ('sql-interactive-mode :popup t)
-       ('magit-diff-mode :below t)
-       ("*shell*" :same t)
-       ("*Help*" :same t)
-       ("*Apropos*" :same t)
-       ("*el-get packages*" :same t)
-       ("*pry*" :same t)
-       ("j-\*" :regexp t :same t))))
   (shackle-mode 1))
