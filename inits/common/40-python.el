0c3e4eeb1525a256097cf0b94b7e1dfe4e2d3529
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-03-13 00:20:48 +0900

    add lxml dash document

--- inits/common/40-python.el
+++ inits/common/40-python.el
@@ -1,5 +1,6 @@
 (add-hook 'python-mode-hook
           '(lambda ()
              (setq-local flycheck-checker 'python-pylint)
-             (setq-local helm-dash-docsets helm-dash-docsets-python-mode)))
+             (setq-local helm-dash-docsets
+                         '("Python 3" "Pandas" "NumPy" "Matplotlib" "Scrapy" "PEPs" "lxml"))))
 (delight 'python-mode " PY")
