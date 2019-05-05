c08c4724383f376c48f8eeb022a2193052a2f1c0
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-05-05 13:04:55 +0900

    add C-o key binding for following nearest link in Info-mode-map

--- inits/common/40-info.el
+++ inits/common/40-info.el
@@ -1,6 +1,8 @@
 (use-package Info
   :delight " IF"
   :defer t
+  :bind (:map Info-mode-map
+              ("C-o" . Info-follow-nearest-node))
   :hook (Info-mode . (lambda ()
                        (buffer-face-set 'recognizable)
                        (toggle-truncate-lines 0)
