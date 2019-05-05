c5c7fe3317b20848d9959297eee8d14584be5584
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-05-05 12:54:28 +0900

    change truncation and identation in Info-mode

--- inits/common/40-info.el
+++ inits/common/40-info.el
@@ -3,4 +3,5 @@
   :defer t
   :hook (Info-mode . (lambda ()
                        (buffer-face-set 'recognizable)
-                       (setq-local truncate-lines nil))))
+                       (toggle-truncate-lines 0)
+                       (adaptive-wrap-prefix-mode 1))))
