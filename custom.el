01249df6c1a443ca09eda5fb65f68b0d487a35d6
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-05-08 16:09:47 +0900

    update custom.el

--- custom.el
+++ custom.el
@@ -4,9 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(enable-remote-dir-locals t)
+ '(org-agenda-files
+   (quote
+    ("~/org/library/feed.org" "/home/tinamori/org/biz_a/biz.org" "/home/tinamori/org/priv_a/devel.org" "/home/tinamori/org/priv_a/home.org" "/home/tinamori/org/priv_a/jikka.org" "/home/tinamori/org/priv_a/life.org")))
  '(org-plantuml-jar-path (format "%s/lib/plantuml/plantuml.jar" env-var-dir))
  '(send-mail-function (quote smtpmail-send-it))
- '(shackle-default-rule (quote (:same t))))
+ '(shackle-default-rule (quote (:same t)))
+ '(smtpmail-default-smtp-server "smtp.daisychain.jp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
