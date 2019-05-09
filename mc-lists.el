5fbad70f0d86244dce65d77bbb9da753f3be2416
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-05-09 14:10:38 +0900

    update mc-lists.el

--- mc-lists.el
+++ mc-lists.el
@@ -3,13 +3,11 @@
 
 (setq mc/cmds-to-run-for-all
       '(
-        org-delete-char
-        org-self-insert-command
-        yas-expand
         ))
 
 (setq mc/cmds-to-run-once
       '(
         hydra-multiple-cursors/mc/mark-more-like-this-extended
         hydra-multiple-cursors/mc/mark-next-like-this
+        hydra-multiple-cursors/my-mc/insert-numbers
         ))
