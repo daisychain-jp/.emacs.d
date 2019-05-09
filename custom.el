dede3937c856bfc3a898b0ad6d9d54ba31f67d7b
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-05-09 14:10:03 +0900

    update custom.el

--- custom.el
+++ custom.el
@@ -4,9 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(enable-remote-dir-locals t)
- '(org-agenda-files
-   (quote
-    ("~/org/library/feed.org" "/home/tinamori/org/biz_a/biz.org" "/home/tinamori/org/priv_a/devel.org" "/home/tinamori/org/priv_a/home.org" "/home/tinamori/org/priv_a/jikka.org" "/home/tinamori/org/priv_a/life.org")))
  '(org-plantuml-jar-path (format "%s/lib/plantuml/plantuml.jar" env-var-dir))
  '(send-mail-function (quote smtpmail-send-it))
  '(shackle-default-rule (quote (:same t)))
