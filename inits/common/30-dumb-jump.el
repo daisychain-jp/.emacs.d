1dfdd41e9332a343b0ebcf8a8493f1edc87d7ea2
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-07-23 21:50:36 +0900

    install dumb-jump mode

---
+++ inits/common/30-dumb-jump.el
@@ -0,0 +1,18 @@
+(use-package dumb-jump
+  :straight t
+  :after (hydra)
+  :init
+  (dumb-jump-mode)
+  :custom
+  (dumb-jump-selector 'ivy)
+  :config
+  (defhydra hydra-projectile (global-map "C-M-g"
+                                         :exit t)
+    "Dumb-Jump"
+    ("g" dumb-jump-go)
+    ("e" dumb-jump-go-prefer-external)
+    ("i" dumb-jump-go-prompt)
+    ("l" dumb-jump-quick-look)
+    ("b" dumb-jump-back)
+    ("C-g" nil "quit")
+    ("q" nil "quit")))
