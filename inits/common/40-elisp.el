075857317580ee8e05eb8b4257a7e8fe71a1ab74
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-07-25 19:04:43 +0900

    give away emacs-lisp-seq-home/end

--- inits/common/40-elisp.el
+++ inits/common/40-elisp.el
@@ -8,25 +8,4 @@
   :hook (emacs-lisp-mode . (lambda ()
                              (setq-local tab-width 4)
                              (setq-local helm-dash-docsets helm-dash-docsets-emacs-lisp-mode)
-                             (eldoc-mode 1)))
-  :after (sequential-command-config)
-  :config
-  ;; emacs-lisp-mode
-  (define-sequential-command emacs-lisp-seq-home
-    beginning-of-line
-    sp-beginning-of-sexp
-    sp-beginning-of-previous-sexp
-    beginning-of-defun
-    beginning-of-buffer
-    seq-return)
-  (define-sequential-command emacs-lisp-seq-end
-    end-of-line
-    sp-end-of-sexp
-    sp-beginning-of-next-sexp
-    end-of-defun
-    end-of-buffer
-    seq-return)
-  (bind-keys :map emacs-lisp-mode-map
-             ("C-a" . emacs-lisp-seq-home)
-             ("C-e" . emacs-lisp-seq-end))
-  )
+                             (eldoc-mode 1))))
