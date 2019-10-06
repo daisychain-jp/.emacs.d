3e508ecdee645a0351de69ac174e4b1fc1ab4a3e
INAMORI Takayuki <t.inamori@daisychain.jp>
2019-10-06 14:57:51 +0900

    set up development environment for python

--- inits/common/40-python.el
+++ inits/common/40-python.el
@@ -1,6 +1,21 @@
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (setq-local flycheck-checker 'python-pylint)
-             (setq-local counsel-dash-docsets
-                         '("Python 3" "Pandas" "NumPy" "Matplotlib" "Scrapy" "PEPs" "lxml"))))
-(delight 'python-mode " PY")
+(use-package python
+  :delight " PY"
+  :after (counsel)
+  :bind (:map python-mode-map
+              ("C-c C-/" . counsel-jedi))
+  :hook (python-mode . (lambda ()
+                         (setq-local flycheck-checker 'python-pylint)
+                         (jedi:setup)
+                         (jedi-mode 1)
+                         (setq-local counsel-dash-docsets
+                                     '("Python 3" "Pandas" "NumPy" "Matplotlib" "Scrapy" "PEPs" "lxml")))))
+
+(use-package jedi
+  :straight t)
+
+(use-package elpy
+  :straight t
+  :custom
+  (elpy-rpc-backend "jedi")
+  :config
+  (elpy-enable))
