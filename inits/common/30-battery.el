573506ff32a788adb75d5440d31d7ddb65f9ec4f
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-05-27 13:57:13 +0900

    mitigate condition of automate shutdown

--- inits/common/30-battery.el
+++ inits/common/30-battery.el
@@ -3,6 +3,8 @@
   (run-with-timer 0 60
                   (lambda ()
                     (let* ((data (and battery-status-function (funcall battery-status-function)))
-                           (percentage (car (read-from-string (cdr (assq ?p data))))))
-                      (when (<= percentage battery-load-critical)
+                           (percentage (car (read-from-string (cdr (assq ?p data)))))
+                           (power (cdr (assq ?L data))))
+                      (when (and (<= percentage battery-load-critical)
+                                 (string= power "BAT"))
                         (save-buffers-kill-emacs t))))))
