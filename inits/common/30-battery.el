170a265c0ae57bde2625a2ddf50aec391c1d3319
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-07-18 09:35:49 +0900

    tighten condition for kill-emacs-auto

--- inits/common/30-battery.el
+++ inits/common/30-battery.el
@@ -1,10 +1,12 @@
 (use-package battery
   :config
-  (run-with-timer 0 60
-                  (lambda ()
-                    (let* ((data (and battery-status-function (funcall battery-status-function)))
-                           (percentage (car (read-from-string (cdr (assq ?p data)))))
-                           (power (cdr (assq ?L data))))
-                      (when (and (<= percentage battery-load-critical)
-                                 (string= power "BAT"))
-                        (save-buffers-kill-emacs t))))))
+  (run-with-timer 60 60 #'kill-emacs-auto))
+
+(defun kill-emacs-auto ()
+  "Kill Emacs if remaining power is under low level."
+  (let* ((data (and battery-status-function (funcall battery-status-function)))
+         (percentage (car (read-from-string (cdr (assq ?p data)))))
+         (power (cdr (assq ?L data))))
+    (when (and (< percentage battery-load-low)
+               (string= power "BAT"))
+      (save-buffers-kill-emacs t))))
