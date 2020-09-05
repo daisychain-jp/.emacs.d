c74d7e797a3811d7a0f94bbdd21615f67503cc09
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-09-05 19:04:53 +0900

    tweak find-dired facitilities

--- inits/common/20-dired.el
+++ inits/common/20-dired.el
@@ -5,8 +5,7 @@
               (")" . dired-hide-details-mode)
               ("o" . dired-omit-mode)
               ("r" . wdired-change-to-wdired-mode)
-              ("C-o" . dired-open)
-              ("C-|" . dired-snatch))
+              ("C-o" . dired-open))
   :custom
   (dired-recursive-copies 'always)
   (dired-recursive-deletes 'top)
@@ -42,14 +41,6 @@
   (:map dired-narrow-map
         ("C-j" . exit-minibuffer)))
 
-(use-package dired-ranger
-  :straight t
-  :bind
-  (:map dired-mode-map
-        ("c" . dired-ranger-copy)
-        ("P" . dired-ranger-paste)
-        ("V" . dired-ranger-move)))
-
 (use-package dired-du
   :straight t
   :diminish ((dired-du-mode . "du"))
@@ -57,6 +48,17 @@
               ("C-M-S-u" . dired-du-mode))
   :custom
   (dired-du-size-format 'comma))
+
+(unbind-key "C-x f")
+(use-package find-dired
+  :bind (:map global-map
+              ("C-x f f" . find-dired)
+              ("C-x f F" . find-lisp-find-dired)
+              ("C-x f g" . find-grep-dired)
+              ("C-x f n" . find-name-dired)
+              ("C-x f d" . find-lisp-find-dired-subdirectories))
+  :custom
+  (find-ls-option '("-print0 | xargs -0 ls -ld" . "-ld")))
 
 (defun dired-open (&optional arg)
   "Open file in Dired.
@@ -69,21 +71,3 @@ Optional argument `ARG' is passed to `open-file'."
       (16 (mapc (lambda (file) (open-file-external file)) files))
       (4 (mapc (lambda (file) (find-file file)) files))
       (t (mapc (lambda (file) (open-file file)) files)))))
-
-(defun dired-snatch ()
-  "Snatch a video file.
-Namely the file is replaced with the compressed as if it's an original.
-
-If directory is chosen, all files in the directory are compressed."
-  (interactive)
-  (lexical-let* ((files (dired-get-marked-files nil nil))
-                 (output-buffer (generate-new-buffer "snatch"))
-                 (proc (progn
-                         (start-process-shell-command "snatch" output-buffer (format "snatch %s" (mapconcat 'identity files " ")))
-                         (get-buffer-process output-buffer))))
-    (if (process-live-p proc)
-        (set-process-sentinel proc (lambda (process signal)
-                                     (when (memq (process-status process) '(exit signal))
-                                       (alert "snatched" :buffer output-buffer :mode 'dired-mode :severity 'normal)
-                                       (kill-buffer output-buffer))))
-      (message "No process running."))))
