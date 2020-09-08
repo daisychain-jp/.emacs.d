97570ef3090851fa45d1b8ce3ebcf5a79fc97efc
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-09-09 08:34:19 +0900

    support calendar-mode and org-agenda-mode to org-record-find-date

--- inits/common/30-org-record.el
+++ inits/common/30-org-record.el
@@ -17,15 +17,26 @@ If optional argument `YEAR' is passed that year's file is returned instead of cu
           org-agenda-files-default))
 (defvar org-record-files (org-record-files))
 (defun org-record-find-date (date)
-  "Find the location of DATE in record file.
+  "Find and visit the location of DATE in record file.
 
-If this called interactively, find today's entry of the datetree."
-  (interactive (let ((date-select (org-read-date)))
-                 (list date-select)))
+DATE must be a string representing the date to find and parsable with `format-time-string'.
+
+If called interactively, it prompt the user to select the date to find."
+  (interactive
+   (cond
+    ((eq major-mode 'calendar-mode)
+     (list (calendar-date-string (calendar-cursor-to-date))))
+    ((eq major-mode 'org-agenda-mode)
+     (let* ((day (or (get-text-property (min (1- (point-max)) (point)) 'day)
+                     (user-error "Don't know which date to open in calendar")))
+            (date (calendar-gregorian-from-absolute day)))
+       (list (calendar-date-string date))))
+    (t (let ((date-select (org-read-date)))
+         (list date-select)))))
   (let* ((d (parse-time-string date))
-         (day (nth 3 d))
-         (month (nth 4 d))
-         (year (nth 5 d)))
+         (day (decoded-time-day d))
+         (month (decoded-time-month d))
+         (year (decoded-time-year d)))
     (find-file (org-record-file year))
     (org-datetree-find-iso-week-create `(,month ,day ,year))))
 (defun org-record-subtree ()
@@ -57,9 +68,12 @@ If this called interactively, find today's entry of the datetree."
 (bind-keys :map org-agenda-mode-map
            ("$" . org-agenda-record-subtree)
            ("&" . org-agenda-ref-id-tieup-tree)
+           ("C" . org-record-find-date)
            :map org-ql-view-map
            ("$" . org-ql-view-record-subtree)
            ("&" . org-agenda-ref-id-tieup-tree)
+           :map calendar-mode-map
+           ("C" . org-record-find-date)
            :map global-map
            ("C-c R" . org-record-search))
 (push '("Record entries" . org-record-search)
