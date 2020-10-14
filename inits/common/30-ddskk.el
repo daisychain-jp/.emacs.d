8f7ddffe3ffcdc770692be5993bded6eec7ddb44
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-10-14 17:44:32 +0900

    change path of jisyo files for ddskk

--- inits/common/30-ddskk.el
+++ inits/common/30-ddskk.el
@@ -7,7 +7,7 @@
   :custom
   (default-input-method "japanese-skk")
   (skk-kakutei-key (kbd "C-m"))
-  (skk-user-directory (format "%s/ddskk" user-emacs-directory))
+  (skk-user-directory (expand-file-name "ddskk" user-emacs-directory))
   (skk-byte-compile-init-file t)
   (skk-tut-file
    (expand-file-name "etc/SKK.tut" (straight--repos-dir "ddskk")))
@@ -39,25 +39,20 @@
   (skk-dcomp-activate nil)
   (skk-dcomp-multiple-activate nil)
   ;; config file
-  (skk-record-file (format "%s/record" skk-data-dir))
-  (skk-emacs-id-file (format "%s/emacs-id" skk-data-dir))
+  (skk-record-file (expand-file-name "record" skk-data-dir))
+  (skk-emacs-id-file (expand-file-name "emacs-id" skk-data-dir))
   ;; jisyo
   (skk-share-private-jisyo t)
+  (skk-compare-jisyo-size-when-saving nil)
   (skk-save-jisyo-instantly t)
   ;; jisyo file/directory
-  (skk-get-jisyo-directory (format "%s/get-jisyo" skk-data-dir))
-  (skk-user-jisyo-directory (format "%s/user-jisyo" skk-data-dir))
-  (skk-jisyo (format "%s/private-jisyo/skk-jisyo.euc-jp" skk-data-dir))
-  (skk-backup-jisyo (format "%s/private-jisyo/skk-jisyo.bak" skk-data-dir))
-  (skk-large-jisyo (concat skk-get-jisyo-directory "/SKK-JISYO.L"))
-  (skk-extra-jisyo-file-list
-   (append
-    (file-expand-wildcards (concat skk-get-jisyo-directory "/SKK-JISYO.*"))
-    (file-expand-wildcards (concat skk-user-jisyo-directory "/SKK-JISYO.*"))))
+  (skk-jisyo (expand-file-name "jisyo" skk-data-dir))
+  (skk-backup-jisyo (expand-file-name "jisyo.bak" skk-data-dir))
+  (skk-large-jisyo (expand-file-name "large-jisyo" skk-data-dir))
   ;; jisyo server
   (skk-server-host "localhost")
   (skk-server-portnum 1178)
   (skk-server-inhibit-startup-server t)
   ;; study
-  (skk-study-file (format "%s/study" skk-data-dir))
-  (skk-study-backup-file (format "%s/study.bak" skk-data-dir)))
+  (skk-study-file (expand-file-name "study" skk-data-dir))
+  (skk-study-backup-file (expand-file-name "study.bak" skk-data-dir)))
