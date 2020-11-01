b703122cf51bc3e96ee3619afcfa821af1833c24
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-11-02 08:17:48 +0900

    add elfeed tag rules for dynamic tagging

---
+++ inits/personal/elfeed-tag-rule.el
@@ -0,0 +1,53 @@
+;; elfeed dynamic tagging rules
+
+(with-eval-after-load 'elfeed
+  ;; for shizuoka-shinbun
+  (add-hook 'elfeed-new-entry-hook
+            (elfeed-make-tagger :feed-url (rx "www.at-s.com")
+                                :entry-link
+                                (rx (or "news/article/social/shizuoka"
+                                        "news/article/politics/shizuoka"
+                                        "news/article/topics/shizuoka"
+                                        "news/article/culture/shizuoka"
+                                        "news/article/local/west"
+                                        "news/article/local/central"
+                                        "news/article/local/east"))
+                                :add 'regional))
+  (add-hook 'elfeed-new-entry-hook
+            (elfeed-make-tagger :feed-url (rx "www.at-s.com")
+                                :entry-link
+                                (rx (or "sports/article/shizuoka"
+                                        "sports/article/national"
+                                        "sports/article/soccer/national"))
+                                :add 'sports))
+  (add-hook 'elfeed-new-entry-hook
+            (elfeed-make-tagger :feed-url (rx "www.at-s.com")
+                                :entry-link
+                                (rx "news/article/science")
+                                :add 'science))
+  (add-hook 'elfeed-new-entry-hook
+            (elfeed-make-tagger :feed-url (rx "www.at-s.com")
+                                :entry-link
+                                (rx (or "news/article/topics/national"
+                                        "news/article/culture/national"
+                                        "news/article/politics/national"
+                                        "news/article/social/national"
+                                        "news/article/economy/national"
+                                        "news/article/health/national"
+                                        "news/article/international"
+                                        "sports/article/sumo"
+                                        "sports/article/golf"
+                                        "sports/article/baseball/npb"
+                                        "sports/article/baseball/mlb"))
+                                :remove 'unread))
+  ;; for favorite entries
+  (add-hook 'elfeed-new-entry-hook
+            (elfeed-make-tagger :feed-url (rx "pc.watch.impress.co.jp")
+                                :entry-title
+                                (rx "【山田祥平のRe:config.sys】")
+                                :add 'valuable))
+  (add-hook 'elfeed-new-entry-hook
+            (elfeed-make-tagger :feed-title (rx "デモクラシータイムス.")
+                                :entry-title
+                                (rx "WeN")
+                                :add 'valuable)))
