baef9f952c040e13055000d2de42f33202e82cfc
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-12-14 17:01:09 +0900

    rebind org-store-link to C-c s

--- inits/common/20-org.el
+++ inits/common/20-org.el
@@ -20,7 +20,8 @@
                                     (file-expand-wildcards (concat env-org-dir "/**/*_a.org")))
     "Default org-agenda-files.")
   :bind (("C-c a" . org-agenda)
-         ("C-c c" . org-capture))
+         ("C-c c" . org-capture)
+         ("C-c s" . org-store-link))
   :init
   (add-to-list 'load-path (concat (file-name-as-directory user-emacs-directory) "straight/repos/org/lisp"))
   (add-to-list 'load-path (concat (file-name-as-directory user-emacs-directory) "straight/repos/org/contrib/lisp"))
