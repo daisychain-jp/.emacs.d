e41b8023e44efc162fa3a40cd06258c3d5577591
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-12-15 09:33:57 +0900

    rebind hydra-parens

--- inits/common/50-paren.el
+++ inits/common/50-paren.el
@@ -9,11 +9,12 @@
   :custom
   (sp-navigate-interactive-always-progress-point t)
   :config
-  (defhydra hydra-smartparens (global-map "M-p"
+  (defhydra hydra-smartparens (global-map "M-("
                                           :foreign-keys warn
                                           :pre (set-face-background 'mode-line "DarkRed")
                                           :post (set-face-background 'mode-line "gray10")
                                           :hint nil)
+    "Parens"
     ;; Moving
     ("a" sp-beginning-of-sexp)
     ("e" sp-end-of-sexp)
@@ -28,8 +29,8 @@
     ("]" sp-forward-slurp-sexp)
     ("}" sp-forward-barf-sexp)
     ;; Wrapping
-    ("r" sp-rewrap-sexp)
-    ("u" sp-unwrap-sexp)
+    ("r" sp-rewrap-sexp :exit t)
+    ("u" sp-unwrap-sexp :exit t)
     ;; ;; Sexp juggling
     ("j" sp-join-sexp)
     ("t" sp-transpose-sexp)
@@ -38,4 +39,8 @@
     ("w" sp-copy-sexp)
     ;; Quit
     ("q" nil)
-    ("g" nil)))
+    ("g" nil))
+  ;; workaround for reverting background color in mode-line
+  (bind-key* "M-)" (lambda ()
+                     (interactive)
+                     (set-face-background 'mode-line "gray10"))))
