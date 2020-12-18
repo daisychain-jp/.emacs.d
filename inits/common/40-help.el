64bfd18bdccdfccd46a755515b90638c37c3010d
INAMORI Takayuki <t.inamori@daisychain.jp>
2020-12-18 16:52:36 +0900

    adopt helpful.el

--- inits/common/40-help.el
+++ inits/common/40-help.el
@@ -8,3 +8,14 @@
                        (buffer-face-set 'recognizable)))
   :config
   (setq-default default-directory (expand-file-name "inits/" user-emacs-directory)))
+
+(use-package helpful
+  :straight t
+  :hook (helpful-mode . (lambda ()
+                          (visual-line-mode 1)
+                          (adaptive-wrap-prefix-mode 1)
+                          (buffer-face-set :family "TakaoGothic" :height 678)))
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  (counsel-descbinds-function #'helpful-key))
