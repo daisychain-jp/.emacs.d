f96e58e1ad5684e2cb93ff6a27691915585e7587
INAMORI Takayuki <t.inamori@daisychain.jp>
2021-01-29 15:36:57 +0900

    chnage font size in info mode

--- inits/common/40-info.el
+++ inits/common/40-info.el
@@ -1,11 +1,10 @@
 (use-package Info
-  :delight " IF"
   :defer t
   :bind (:map Info-mode-map
               ("C-j" . Info-follow-nearest-node)
               ("C-c C-o" . Info-follow-nearest-node)
               ("a"   . info-apropos))
   :hook (Info-mode . (lambda ()
-                       (buffer-face-set 'recognizable)
+                       (buffer-face-set :family "TakaoGothic" :height 678)
                        (visual-line-mode 1)
                        (adaptive-wrap-prefix-mode 1))))
