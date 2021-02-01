7f6e5040938c4f95f4ad99f1fa8fe4a3076c66fb
INAMORI Takayuki <t.inamori@daisychain.jp>
2021-02-01 13:16:39 +0900

    refine and rebind calendar-org-archive-show-summary-day

--- inits/common/10-calendar.el
+++ inits/common/10-calendar.el
@@ -1,25 +1,32 @@
 (use-package calendar
   :bind (:map calendar-mode-map
-              ("v" . calendar-org-record-show-date))
+              ("v" . calendar-org-archive-show-summary-day))
   :hook (calendar-mode . (lambda ()
-                           (buffer-face-set 'calendar)))
-  :config
-  (defun calendar-org-record-show-date ()
-    "docstring"
-    (interactive)
-    (let* ((display-buffer-alist
-            '(("\\*Org QL View\\*"
-               (display-buffer-reuse-window display-buffer-at-bottom)
-               )))
-           (calendar-date-display-form calendar-iso-date-display-form)
-           (date (calendar-date-string (calendar-cursor-to-date)))
-           (d (parse-time-string date))
-           (year (decoded-time-year d))
-           (files (org-record-file year)))
-      (org-ql-search files `(and (parent (heading ,date))
-                                 (or (tags "journal")
-                                     (tags "ac_make"))))
-      (other-window 1))))
+                           (buffer-face-set 'calendar))))
+
+(defun calendar-org-archive-show-summary-day ()
+  "Open a new window attached to the calendar view \
+and show summary of the day which the cursor is pointing to in the calendar view."
+  (interactive)
+  (let* ((buf-name "Summary of the Day")
+         (org-ql-view-buffer buf-name)
+         (display-buffer-alist
+          `((,buf-name
+             (display-buffer-reuse-window display-buffer-at-bottom))))
+         (calendar-date-display-form calendar-iso-date-display-form)
+         (date (calendar-date-string (calendar-cursor-to-date)))
+         (d (parse-time-string date))
+         (year (decoded-time-year d))
+         (files (org-record-file year)))
+    (org-ql-search files `(and (parent (heading ,date))
+                               (or (tags "journal")
+                                   (tags "ac_make")
+                                   (and (tags "ac_read")
+                                        (not (tags "episode")))
+                                   (tags "ac_cook")
+                                   (tags "ac_purchase")
+                                   (tags "ac_solve"))))
+    (other-window 1)))
 
 ;; include japanese holidays
 (use-package japanese-holidays
