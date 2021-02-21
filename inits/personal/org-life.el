a55e2779654036bea98d8ca0981dd48ad27f89ee
INAMORI Takayuki <t.inamori@daisychain.jp>
2021-02-21 12:22:49 +0900

    detach habits taken on today in daily task list

--- inits/personal/org-life.el
+++ inits/personal/org-life.el
@@ -119,11 +119,8 @@
                                      (not (or (todo ,org-todo-keyword-2)
                                               (habit) (done))))
                                ((org-ql-block-header "Today's common event")))
-          (org-ql-search-block '(or (and (done)
-                                         (closed :on today))
-                                    (and (habit)
-                                         (clocked :on today)
-                                         (planning :from 1)))
+          (org-ql-search-block '(and (done)
+                                     (closed :on today))
                                ((org-ql-block-header "Completed tasks on today")))))
         ("g" "This Week's agenda"
          ((org-ql-search-block `(heading ,(format-time-string "%G-W%V"))
