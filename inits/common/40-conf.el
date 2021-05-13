db56824d2dee0df78b92f538e18ad81ca1026808
Kona-Yuki-SAN <6841207+p-snow@users.noreply.github.com>
2021-05-13 11:26:50 +0900

    tweak for conf-xxx-mode

--- inits/common/40-conf.el
+++ inits/common/40-conf.el
@@ -1,3 +1,17 @@
+(use-package conf-mode
+  :hook
+  ((conf-space-mode
+    conf-unix-mode
+    conf-ppd-mode
+    conf-colon-mode
+    conf-xdefaults-mode
+    conf-javaprop-mode
+    conf-toml-mode
+    conf-windows-mode
+    conf-desktop-mode)
+   . (lambda ()
+       (toggle-truncate-lines 1))))
+
 (use-package generic-x
   :mode (("\\.conf$"     . apache-conf-generic-mode)
          ("\\.htaccess$" . apache-conf-generic-mode))
@@ -6,6 +20,7 @@
   (apache-conf-generic-mode " GE")
   (alias-generic-mode       " GE")
   :config
+  (toggle-truncate-lines 1)
   (add-hook 'after-change-major-mode-hook
             (lambda ()
               (when (eq major-mode 'apache-conf-generic-mode)
