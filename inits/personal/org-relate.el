44c38a85e739717c1cf1e6391c19472103be9477
Kona-Yuki-SAN <6841207+p-snow@users.noreply.github.com>
2021-06-12 10:04:57 +0900

    rewrite search function in org-relate

--- inits/personal/org-relate.el
+++ inits/personal/org-relate.el
@@ -10,8 +10,12 @@
   (file-expand-wildcards (concat env-org-dir "/**/*.org"))
   "A list of target files to search relation nodes.")
 
+(with-eval-after-load 'org
+  (bind-key "C-c %" #'org-relate-search
+            org-mode-map))
+
 (with-eval-after-load 'org-keys
-  (setf (map-elt org-speed-commands "%") #'org-relate-look)
+  (setf (map-elt org-speed-commands "%") #'org-relate-search)
   (setf (map-elt org-speed-commands "&") #'org-relate-interrelate)
   (push '("Org-Relate Operation") org-speed-commands))
 
@@ -19,52 +23,78 @@
   (bind-keys :map org-agenda-mode-map
              ("&" . org-agenda-relation-interrelate)))
 
-(defun org-relate-look ()
-  "Entry command for org-relate-look-up, org-relate-look-down
-and org-relate-look-around."
-  (interactive)
-  (let ((char (read-char-from-minibuffer "Select look-[u]p/[d]own/a[r]ound")))
-    (cond
-     ((equal char ?u) (org-relate-look-up))
-     ((equal char ?d) (org-relate-look-down))
-     ((equal char ?r) (org-relate-look-around)))))
+(defun org-relate-search ()
+  "Search for entries related to the entry at point.
 
-(defun org-relate-look-down ()
-  "Show all child nodes of this entry."
+User who would like to search for parent and children of that, namely siblings,
+ must press 'p'.
+User who would like to search for children of this entry must press 'c'.
+User who would like to search for friends either pointing to this entry
+ or pointed from this entry must be press 'f'.
+User who would like to search for all listed above must press 'a'."
   (interactive)
-  (if-let* ((id (org-id-get))
-            (rel-prop-link (format "[[id:%s]]" id)))
-      (org-ql-search
-        org-relate-target-files
-        `(or (property ,org-relate-property ,id)
-             (property ,org-relate-property ,rel-prop-link)))
-    (message "This entry has no 'ID' property")))
+  (let* ((org-super-agenda-properties-inherit nil)
+         (char (read-char-from-minibuffer "Target: [a]ll [p]arent [c]hild [f]riend"))
+         (queries (seq-remove 'null
+                              (cond
+                               ((equal char ?a) (list (org-relate--search-parent-org-ql-query)
+                                                      (org-relate--search-children-org-ql-query)
+                                                      (org-relate--search-friends-refer-org-ql-query)
+                                                      (org-relate--search-friends-referred-org-ql-query)
+                                                      (org-relate--search-siblings-org-ql-query)))
+                               ((equal char ?p) (list (org-relate--search-parent-org-ql-query)
+                                                      (org-relate--search-siblings-org-ql-query)))
+                               ((equal char ?c) (list (org-relate--search-children-org-ql-query)))
+                               ((equal char ?f) (list (org-relate--search-friends-refer-org-ql-query)
+                                                      (org-relate--search-friends-referred-org-ql-query)))))))
+    (if queries
+        (org-ql-search org-relate-target-files
+          (seq-reduce (lambda (accum elem)
+                        (push elem (cdr (last accum)))
+                        accum)
+                      (mapcar 'car queries)
+                      '(or))
+          :super-groups (mapcar 'cdr queries))
+      (message "No query found"))))
 
-(defun org-relate-look-up ()
-  "Show parent node and all child nodes of it (siblings)."
-  (interactive)
-  (let ((org-super-agenda-properties-inherit nil))
-    (if-let* ((prop-ref (org-entry-get (point) org-relate-property))
-              (index (string-match (format "\\(%s\\)" thing-at-point-uuid-regexp) prop-ref))
-              (parent-id (match-string 0 prop-ref))
-              (parent-id-link (format "[[id:%s]]" parent-id)))
-        (org-ql-search
-          org-relate-target-files
-          `(or (property "ID" ,parent-id)
-               (property ,org-relate-property ,parent-id)
-               (property ,org-relate-property ,parent-id-link)) ; for backward compatibility)
-          :super-groups `((:name "Parent" :property ("ID" ,parent-id))))
-      (message "This entry has no parent"))))
+(defun org-relate--search-children-org-ql-query ()
+  (when-let* ((id (org-id-get))
+              (rel-prop-link (format "[[id:%s]]" id)))
+    `((or (property ,org-relate-property ,id)
+          (property ,org-relate-property ,rel-prop-link)) .
+          (:name "Children" :property (,org-relate-property ,id)))))
 
-(defun org-relate-look-around ()
-  "Show all entries refer to this node by having an org-link which contain this node's ID."
-  (interactive)
-  (if-let* ((id (org-id-get))
-            (ref-re (format "\\[\\[id:%s\\].*\\]" id)))
-      (org-ql-search
-        org-relate-target-files
-        `(regexp ,ref-re))
-    (message "This entry has no 'ID' property")))
+(defun org-relate--search-parent-org-ql-query ()
+  (if-let* ((prop-ref (org-entry-get (point) org-relate-property))
+            (index (string-match (format "\\(%s\\)" thing-at-point-uuid-regexp) prop-ref))
+            (parent-id (match-string 0 prop-ref)))
+      `((property "ID" ,parent-id) .
+        (:name "Parent" :property ("ID" ,parent-id)))))
+
+(defun org-relate--search-siblings-org-ql-query ()
+  (if-let* ((prop-ref (org-entry-get (point) org-relate-property))
+            (index (string-match (format "\\(%s\\)" thing-at-point-uuid-regexp) prop-ref))
+            (parent-id (match-string 0 prop-ref))
+            (parent-id-link (format "[[id:%s]]" parent-id)))
+      `((or (property ,org-relate-property ,parent-id)
+            (property ,org-relate-property ,parent-id-link)) .
+            (:name "Siblings" :property (,org-relate-property ,parent-id)))))
+
+(defun org-relate--search-friends-refer-org-ql-query ()
+  (when-let* ((id (org-id-get))
+              (ref-re (format "\\[\\[id:%s\\].*\\]" id)))
+    `((regexp ,ref-re) .
+      (:name "Friends pointing to this entry" :regexp ,ref-re))))
+
+(defun org-relate--search-friends-referred-org-ql-query ()
+  (let* ((id-link-re (format "\\[\\[id:\\(%s\\)\\].*\\]" thing-at-point-uuid-regexp))
+         (end (or (save-excursion
+                    (outline-next-heading))
+                  (point-max))))
+    (when (save-excursion
+            (re-search-forward id-link-re end t))
+      `((property "ID" ,(match-string 1)) .
+        (:name "Friends to whom this entry is pointing" :property ("ID" ,(match-string 1)))))))
 
 (defun org-relate-interrelate ()
   "Make parent-child relationship by inserting property.
