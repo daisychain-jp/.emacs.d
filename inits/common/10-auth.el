2e663fbac250a0916751e409dd38974688f07275
p-snow <6841207+p-snow@users.noreply.github.com>
2021-06-14 19:53:16 +0900

    add packages for password-store

--- inits/common/10-auth.el
+++ inits/common/10-auth.el
@@ -6,3 +6,14 @@
 (use-package auth-source-pass
   :config
   (auth-source-pass-enable))
+
+(use-package password-store
+  :straight t
+  :custom
+  (password-store-time-before-clipboard-restore 25))
+
+(use-package pass
+  :straight t
+  :config
+  (bind-keys :map pass-mode-map
+             ("C-j" . pass-view)))
